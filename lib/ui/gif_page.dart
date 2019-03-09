69ca15e15eefdb2634ccac4e9c35574b8b39fb58
tresende <thiago.gcresende@gmail.com>
2019-03-09 11:42:43 -0300

    Botao de compartilhamento

--- lib/ui/gif_page.dart
+++ lib/ui/gif_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-
+import 'package:share/share.dart';
+  
 class GifPage extends StatelessWidget {
   final Map _gifData;
 
@@ -7,14 +8,23 @@ class GifPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    String imageUrl = this._gifData['images']['fixed_height']['url'];
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         backgroundColor: Colors.black,
         title: Text("teste"),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.share),
+            onPressed: () {
+              Share.share(imageUrl);
+            },
+          )
+        ],
       ),
       body: Center(
-        child: Image.network(this._gifData['images']['fixed_height']['url']),
+        child: Image.network(imageUrl),
       ),
     );
   }
